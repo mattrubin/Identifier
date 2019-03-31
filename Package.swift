b521a4a28fdea18a5d803053cf449515f398bdfc
Matt Rubin <opensource@mattrubin.me>
2019-03-31 02:06:02 -0400

    Specify platform deployment targets in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "Identifier",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9),
+        .watchOS(.v2),
+    ],
     products: [
         .library(name: "Identifier", targets: ["Identifier"]),
     ],

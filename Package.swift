025ac3c6af23d0a4d6f73dd9fb7b6899a7c15b05
Matt Rubin <opensource@mattrubin.me>
2024-12-08 14:30:49 -0500

    Bump Swift package tools and platform versions and fix warnings

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version:5.0
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "Identifier",
     platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9),
-        .watchOS(.v2),
+        .iOS(.v13),
+        .macOS(.v10_13),
+        .tvOS(.v13),
+        .visionOS(.v1),
+        .watchOS(.v4),
     ],
     products: [
         .library(name: "Identifier", targets: ["Identifier"]),
     ],
     targets: [
-        .target(name: "Identifier"),
-        .testTarget(name: "IdentifierTests", dependencies: ["Identifier"]),
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(name: "Identifier", exclude: ["Info.plist"]),
+        .testTarget(name: "IdentifierTests", dependencies: ["Identifier"], exclude: ["Info.plist"]),
+    ]
 )

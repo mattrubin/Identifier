c619848578908235688d859e219bc902b03b174c
Matt Rubin <opensource@mattrubin.me>
2026-04-26 20:00:09 -0400

    Drop explicit platforms from the package manifest
    As per https://forums.swift.org/t/swift-package-manager-and-supported-platforms-avoiding-available-macos-10-15-0/59882, the platforms array *doesn't* limit the platforms to only those listed, and omitting this parameter lets SPM use its own minimum supported deployment targets.
    
    From https://docs.swift.org/swiftpm/documentation/packagedescription/supportedplatform/
    > By default, Swift Package Manager assigns a predefined minimum deployment version for each supported platforms unless you configure supported platforms using the `platforms` API. This predefined deployment version is the oldest deployment target version that the installed SDK supports for a given platform.

--- Package.swift
+++ Package.swift
@@ -4,13 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "Identifier",
-    platforms: [
-        .iOS(.v15),
-        .macOS(.v11),
-        .tvOS(.v15),
-        .visionOS(.v1),
-        .watchOS(.v8),
-    ],
     products: [
         .library(name: "Identifier", targets: ["Identifier"]),
     ],
